479579696f0dff65b5b23a290da3e50843dc3884
HamSungJun <wleo12345@naver.com>
2018-10-06 19:08:48 +0900

    카운터 , 서버 , 태스크노드 일부 구현

---
+++ src/TaskNode.java
@@ -0,0 +1,14 @@
+public class TaskNode {
+    public String chickenName;
+    public int cookingTime;
+    public int tableNum;
+    public int orderNum;
+
+    TaskNode(String chickenName , int cookingTime , int tableNum , int orderNum){
+        this.chickenName = chickenName;
+        this.cookingTime = cookingTime;
+        this.tableNum = tableNum;
+        this.orderNum = orderNum;
+    }
+
+}
