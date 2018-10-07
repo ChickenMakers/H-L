10808b2dcf9dbc839d86cb3409249fe8dcf9d0e2
HamSungJun <wleo12345@naver.com>
2018-10-07 16:05:40 +0900

    클래스 컴포넌트 연결중

--- src/Server.java
+++ src/Server.java
@@ -2,8 +2,10 @@ public class Server {
 
     private int lastGuiededSeat = -1;
     public Seats chickenSeats;
+    public Counter counter;
     Server(){
         chickenSeats = new Seats();
+        counter = new Counter(this);
     }
 
     public int findFreeSeat(Clients targetClient){
@@ -12,9 +14,9 @@ public class Server {
 
         while (true){
 
-            this.lastGuiededSeat = (++this.lastGuiededSeat % 16);
-            int row = (this.lastGuiededSeat / 4);
-            int col = (this.lastGuiededSeat % 4);
+            this.lastGuiededSeat = (++this.lastGuiededSeat % this.chickenSeats.getTableCount());
+            int row = (this.lastGuiededSeat / this.chickenSeats.getN());
+            int col = (this.lastGuiededSeat % this.chickenSeats.getN());
 
             if(seatState[row][col] == null){
                 seatState[row][col] = targetClient;
@@ -26,5 +28,9 @@ public class Server {
                 return -1;
             }
         }
+    }
+    public void cleanTable(int row , int col){
+        this.chickenSeats.cleanTable(row,col);
+        System.out.println((row*this.chickenSeats.getN())+(col)+"번 테이블을 정리하였습니다.");
     }
 }
