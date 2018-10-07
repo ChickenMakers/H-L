a418372a14e4cea987efd983202e05eb4882584a
HamSungJun <wleo12345@naver.com>
2018-10-07 18:07:30 +0900

    DB 작업

--- src/Counter.java
+++ src/Counter.java
@@ -1,7 +1,10 @@
+
 public class Counter {
     private Server server;
+    private DB_TRANSACTION MYDB;
     Counter(Server server){
         this.server = server;
+        this.MYDB = new DB_TRANSACTION();
     }
     public void getPaid(TaskNode taskInfo , int numOfClients){
 
@@ -9,9 +12,10 @@ public class Counter {
         int row = (taskInfo.tableNum / this.server.chickenSeats.getN());
         int col = (taskInfo.tableNum % this.server.chickenSeats.getN());
         this.server.cleanTable(row,col);
+        this.writeLog(taskInfo,numOfClients);
 
     }
-//    private void writeLog(TaskNode taskInfo , int numOfClients){
-//
-//    }
+    private void writeLog(TaskNode taskInfo , int numOfClients){
+
+    }
 }
